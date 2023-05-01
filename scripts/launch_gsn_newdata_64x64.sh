ae29d2e5fcf6d424dc3f97caf0faf4b9f637092f
zhewenhu <larried1111@gmail.com>
2023-05-01 01:18:58 -0500

    sbatch run

--- scripts/launch_gsn_newdata_64x64.sh
+++ scripts/launch_gsn_newdata_64x64.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0,1 python train_gsn.py \
+CUDA_VISIBLE_DEVICES=0,1 python3.8 train_gsn.py \
 --base_config 'configs/models/gsn_new_config.yaml' \
 --log_dir 'logs' \
 data_config.dataset='new' \
